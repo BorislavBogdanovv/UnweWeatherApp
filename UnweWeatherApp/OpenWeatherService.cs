36fc6512b9a0c489b0896665055382cb48c7c812
Borislav Bogdanov <105934070+BorislavBogdanovv@users.noreply.github.com>
2022-06-03 13:22:33 +0300

    new class

---
+++ UnweWeatherApp/OpenWeatherService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace UnweWeatherApp
+{
+    public class OpenWeatherService
+    {
+        HttpClient_client;
+
+        public OpenWeatherService()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<WeatherData> GetWeatherData(string query)
+        {
+            WeatherData weatherData = null;
+
+            try
+            {
+                var response = await_client.GetAsync(query);
+
+                if (response.IsSuccesStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\t\tERROR {0}", ex.Message);
+            }
+
+            return WeatherData;
+        }
+    }
+}
